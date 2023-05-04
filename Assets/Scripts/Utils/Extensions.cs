feb497d86ac9ac8ac70fe795449a73cba6e14168
jason7599 <jason7599@hanmail.net>
2023-05-04 23:33:45 +0900

    Added more extension methods for transform

--- Assets/Scripts/Utils/Extensions.cs
+++ Assets/Scripts/Utils/Extensions.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public static class TransformEx
 {
-    // linear
+    // Smooth, linear move to destination for duration seconds 
     public static IEnumerator SmoothMove(this Transform transform, float duration, Vector3 dest)
     {
         Vector3 start = transform.position;
@@ -16,5 +16,22 @@ public static class TransformEx
             yield return null;
         }
         transform.position = dest;
+    }
+
+    public static void LerpLookRotation(this Transform transform, Transform target, float interpolation)
+    {
+        LerpLookRotation(transform, target.position, interpolation);
+    }
+
+    public static void LerpLookRotation(this Transform transform, Vector3 pos, float interpolation)
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(pos - transform.position), interpolation);
+    }
+
+    public static Vector3 DirectionToPlayerHeightCorrected(this Transform transform)
+    {
+        Vector3 dir = Player.Position - transform.position; 
+        dir.y = 0;
+        return dir;
     }
 }
