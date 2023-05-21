2239ed6dabd5806d162cd448a6071ac6de53dace
jason7599 <jason7599@hanmail.net>
2023-05-21 23:04:30 +0900

    Ghost level up on successful attack

--- Assets/Scripts/Enemies/Ghost.cs
+++ Assets/Scripts/Enemies/Ghost.cs
@@ -12,27 +12,38 @@ public class Ghost : MonoBehaviour
         Attack,     // ! Lunge at player
     }
 
+    [SerializeField] private ParticleSystem _spawnEffect;
+    [SerializeField] private ParticleSystem _passiveEffect;
+
     [SerializeField] private float _initialSpawnTimeThreshold = 25f; // how many seconds of flashlight off it takes for ghost to spawn
     [SerializeField] private float _spawnTimerChargeRate = 1f;
     [SerializeField] private float _spawnTimerDegradeRate = .5f;
 
-    [SerializeField] private float _speed = 3f;
+    [SerializeField] private float _initialSpeed = 3f;
 
+    [SerializeField] private int _initialAttackDamage = 10;
     [SerializeField] private float _attackRange = 4f;
     [SerializeField] private float _attackDuration = 1f;
     [SerializeField] private float _attackChargeUpDuration = .75f; // charge attack
     [SerializeField] private float _attackCooldownDuration = 1f; // if attack misses, cooldown
-    
-    [SerializeField] private ParticleSystem _spawnEffect;
-    [SerializeField] private ParticleSystem _passiveEffect;
+
+    // "level up" every successful attack
+    [SerializeField] private int _maxLevel = 5;
+    [SerializeField] private float _spawnTimeThresholdIncrement = 5f;
+    [SerializeField] private float _speedIncrement = 1f;
+    [SerializeField] private int _attackDamageIncrement = 10;
+
+    private float _spawnTimeThreshold;
+    private float _speed;
+    private int _attackDamage;
+    private int _level = 1;
 
     private State _state;
-    
     private float _spawnTimer;
-    private float _spawnTimeThreshold;
 
     private Material _material;
     private Collider _hitDetectCollider; // layer: playertrigger. enabled before attacking to check if attack hit
+    private Coroutine _attackRoutine;
 
     [Header("TEMP SHIT")]
     [SerializeField] private Slider _spawnSlider;
@@ -45,6 +56,9 @@ public class Ghost : MonoBehaviour
 
         _spawnTimeThreshold = _initialSpawnTimeThreshold;
         _spawnSlider.maxValue = _initialSpawnTimeThreshold;
+
+        _speed = _initialSpeed;
+        _attackDamage = _initialAttackDamage;
         
         GoToSleep();
     }
@@ -91,7 +105,7 @@ public class Ghost : MonoBehaviour
 
         if (transform.DistanceToPlayerHeightCorrected() <= _attackRange)
         {
-            StartCoroutine(AttackRoutine());
+            _attackRoutine = StartCoroutine(AttackRoutine());
         }
         else
         {
@@ -105,7 +119,8 @@ public class Ghost : MonoBehaviour
         _spawnEffect.Play();
         _passiveEffect.Play();
 
-        // transform.position = Player.Position + Random.on
+        // TODO: Spawn where player is looking at
+        transform.position = Player.Position;
 
         StartCoroutine(SpawnRoutine());
     }
@@ -154,12 +169,24 @@ public class Ghost : MonoBehaviour
         _state = State.Chase;
     }
 
-    private void OnTriggerEnter(Collider collider) // sucessful hit. Stop all coroutines 
+    private void OnTriggerEnter(Collider collider) // sucessful hit.
     {
         print("ghost hit!");
 
         _hitDetectCollider.enabled = false;
-        StopAllCoroutines();
+        StopCoroutine(_attackRoutine);
+
+        Player.Health.TakeDamage(_attackDamage);
+
+        if (_level != _maxLevel)
+        {
+            _level++;
+            _spawnTimeThreshold += _spawnTimeThresholdIncrement;
+            _spawnSlider.maxValue = _spawnTimeThreshold;
+
+            _attackDamage += _attackDamageIncrement;
+            _speed += _speedIncrement;
+        }
 
         GoToSleep();
     }
