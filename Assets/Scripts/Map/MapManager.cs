e9aadcbb3cba21b03c883fa700e4c3943117e255
Ranccat <wogurjo98@gmail.com>
2023-06-30 00:29:34 +0900

    Added GetBackPos()

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -8,6 +8,7 @@ public class MapManager : MonoBehaviour
     public static MapManager instance;
     
     private static int[,] maze;
+    public Camera cam;
     
     [SerializeField] private MazeConfiguration mazeConfig;
     [SerializeField] private NavMeshSurface surface;
@@ -18,6 +19,14 @@ public class MapManager : MonoBehaviour
         
         maze = MapGenerator.InitMap(mazeConfig);
         surface.BuildNavMesh();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GetBackPos();
+        }
     }
 
     public Vector3 GetRandomPos()
@@ -47,5 +56,110 @@ public class MapManager : MonoBehaviour
         GameObject monster = monsterPrefabs[UnityEngine.Random.Range(0, monsterNum)];
         Vector3 spawnLocation = new Vector3(mapSize * wallSize, 0, mapSize * wallSize + 3);
         Instantiate(monster, spawnLocation, Quaternion.identity);
+    }
+
+    public Vector3 GetBackPos()
+    {
+        int wallSize = mazeConfig.wallSize;
+        
+        Vector3 playerPos = Player.Position;
+        int posX = Convert.ToInt32(Math.Round(playerPos.x / 3));
+        int posY = Convert.ToInt32(Math.Round(playerPos.z / 3));
+
+        Vector3 lookDir = Player.Transform.forward;
+        double lookX = lookDir.x;
+        double lookY = lookDir.z;
+        
+        Tuple<int, int> firstDir;
+        Tuple<int, int> secondDir;
+        Tuple<int, int> thirdDir;
+        
+        // Quadrant 1
+        if (lookX >= 0 && lookY >= 0)
+        {
+            if (lookX >= lookY)
+            {
+                firstDir = new Tuple<int, int>(-1, 0);
+                secondDir = new Tuple<int, int>(0, -1);
+                thirdDir = new Tuple<int, int>(0, 1);
+            }
+            else
+            {
+                firstDir = new Tuple<int, int>(0, -1);
+                secondDir = new Tuple<int, int>(-1, 0);
+                thirdDir = new Tuple<int, int>(1, 0);
+            }
+        }
+        // Quadrant 2
+        else if (lookX < 0 && lookY >= 0)
+        {
+            if (Math.Abs(lookX) <= Math.Abs(lookY))
+            {
+                firstDir = new Tuple<int, int>(0, -1);
+                secondDir = new Tuple<int, int>(1, 0);
+                thirdDir = new Tuple<int, int>(-1, 0);
+            }
+            else
+            {
+                firstDir = new Tuple<int, int>(1, 0);
+                secondDir = new Tuple<int, int>(0, -1);
+                thirdDir = new Tuple<int, int>(0, 1);
+            }
+        }
+        // Quadrant 3
+        else if (lookX < 0 && lookY < 0)
+        {
+            if (Math.Abs(lookX) >= Math.Abs(lookY))
+            {
+                firstDir = new Tuple<int, int>(1, 0);
+                secondDir = new Tuple<int, int>(0, 1);
+                thirdDir = new Tuple<int, int>(0, -1);
+            }
+            else
+            {
+                firstDir = new Tuple<int, int>(0, 1);
+                secondDir = new Tuple<int, int>(1, 0);
+                thirdDir = new Tuple<int, int>(-1, 0);
+            }
+        }
+        // Quadrant 4
+        else
+        {
+            if (Math.Abs(lookX) >= Math.Abs(lookY))
+            {
+                firstDir = new Tuple<int, int>(-1, 0);
+                secondDir = new Tuple<int, int>(0, 1);
+                thirdDir = new Tuple<int, int>(0, -1);
+            }
+            else
+            {
+                firstDir = new Tuple<int, int>(0, 1);
+                secondDir = new Tuple<int, int>(-1, 0);
+                thirdDir = new Tuple<int, int>(1, 0);
+            }
+        }
+
+        int x = posX + firstDir.Item1;
+        int y = posY + firstDir.Item2;
+        if (maze[x, y] == 0)
+        {
+            return new Vector3(x * wallSize, 0, y * wallSize);
+        }
+        
+        x = posX + secondDir.Item1;
+        y = posY + secondDir.Item2;
+        if (maze[x, y] == 0)
+        {
+            return new Vector3(x * wallSize, 0, y * wallSize);
+        }
+        
+        x = posX + thirdDir.Item1;
+        y = posY + thirdDir.Item2;
+        if (maze[x, y] == 0)
+        {
+            return new Vector3(x * wallSize, 0, y * wallSize);
+        }
+
+        return new Vector3(-1, -1, -1);
     }
 }
